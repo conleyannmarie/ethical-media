63b0d58052e593e2c4bda30003d7885f914abeb6
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-10-28 20:34:13 -0600

    index.js maybe done

--- models/index.js
+++ models/index.js
@@ -3,23 +3,43 @@ const Rating = require('./Rating');
 const Category = require('./Category');
 const People = require('./People');
 
-People.hasMany(Rating, {
-    foreignKey: ''
+// People.hasMany(Rating, {
+//     foreignKey: ''
+// })
+
+Category.belongTo(Rating, {
+    foreignKey: 'rating_id'
+})
+
+Category.hasMany(Rating, {
+    foreignKey: 'category_id'
+})
+
+Category.belongTo(People, {
+    foreignKey: 'people_id'
 })
 
 People.hasMany(Category, {
-    foreignKey: ''
+    foreignKey: 'people_id'
 })
 
-People.belongsToMany(Rating, {
-    through: Category,
-    as: '',
-    foreignKey: ''
+
+Rating.belongTo(User, {
+    foreignKey: 'user_id'
 })
 
-// People.belongsToMany(Category {
-//     through: 
-// })
+User.hasMany(Rating, {
+    foreignKey: 'rating_id'
+})
+
+
+
+
+
+
+
+
+
 
 
 module.exports = { User, Rating, Category, People };
