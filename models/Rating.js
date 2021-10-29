2580af2c0ba65986b0213ab0d93fc2461ff43c32
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-28 20:05:19 -0600

    added rating page:

---
+++ models/Rating.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize')
+const { model } = require('../config/connection')
+const sequelize = require('../config/connection')
+
+class Rating extends Model {}
+
+Rating.init(
+{
+    id: { 
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+
+    },
+        rated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        rating_for: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isNumeric: true,
+                min: 1,
+                max: 10
+            }
+
+        },
+        about_rating: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'rating'
+    }
+    
+);
+
+model.exports = Rating;
