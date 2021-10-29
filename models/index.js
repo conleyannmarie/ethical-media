1f56add1fe042479652c047c349de1e01174fdce
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-10-28 19:54:51 -0600

    Please look at models/index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,17 @@
 const User = require('./User');
+const Rating = require('./Rating');
+const Category = require('./Category');
+const People = require('./People');
+
+People.hasMany(Rating, {
+    foreignKey: ''
+})
+
+People.hasMany(Category, {
+    foreignKey: ''
+})
+
+
+
 
 module.exports = { User };
