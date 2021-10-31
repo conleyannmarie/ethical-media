56785718060dad40571156ff8dcc5e15f93f2be7
Tom Hertig <joehertig94@gmail.com>
2021-10-31 15:35:59 -0600

    created/added profile card

--- html/script.js
+++ html/script.js
@@ -1,14 +1,22 @@
-// const panels = document.querySelectorAll('.panel')
+const ratingStars = [...document.getElementsByClassName("rating-star")];
 
-// panels.forEach((panel) => {
-//     panel.addEventListener('click', () => {
-//         removeActiveClasses()
-//         panel.classList.add('active')
-//     })
-// })
+function executeRating(stars) {
+  const starClassActive = "rating-star fas fa-star";
+  const starClassInactive = "rating-star far fa-star";
+  const starsLength = stars.length;
+  let i;
 
-// function removeActiveClasses() {
-//     panels.forEach(panel => {
-//         panel.classList.remove('active')
-//     })
-// }git remote add origin git@github.com:conleyannmarie/ethical-media.git
+  stars.map((star) => {
+    star.onclick = () => {
+      i = stars.indexOf(star);
+
+      if (star.className === starClassInactive) {
+        for (i; i >= 0; --i) stars[i].className = starClassActive;
+      } else {
+        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+      }
+    };
+  });
+}
+
+executeRating(ratingStars);
