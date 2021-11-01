fee9df7d86d842f22057fb80351d381e0c1587c2
Tom Hertig <joehertig94@gmail.com>
2021-11-01 17:16:07 -0600

    created work, love, friendships, family form/need to fix bug

--- html/script.js
+++ html/script.js
@@ -1,22 +1,9 @@
-const ratingStars = [...document.getElementsByClassName("rating-star")];
-
-function executeRating(stars) {
-  const starClassActive = "rating-star fas fa-star";
-  const starClassInactive = "rating-star far fa-star";
-  const starsLength = stars.length;
-  let i;
-
-  stars.map((star) => {
-    star.onclick = () => {
-      i = stars.indexOf(star);
-
-      if (star.className === starClassInactive) {
-        for (i; i >= 0; --i) stars[i].className = starClassActive;
-      } else {
-        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
-      }
-    };
-  });
-}
-
-executeRating(ratingStars);
+const btn = document.querySelector(".submit-rating");
+const thanksmsg = document.querySelector(".thanks-msg");
+const starRating = document.querySelector(".star-input");
+// Success msg show/hide
+btn.onclick = () => {
+  starRating.style.display = "none";
+  thanksmsg.style.display = "table";
+  return false;
+};
