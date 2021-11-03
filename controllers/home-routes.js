0c953f86ca751aa4e420c28e3df7202f3cdaaddf
bhyland <blakehyland95@gmail.com>
2021-11-03 13:41:34 -0600

    added homepage routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,114 @@
 const router = require('express').Router();
+const { Rating, User, Category } = require('../models');
 
+//returns all users
 router.get('/', (req, res) => {
+    User.findAll({
+        attributes:
+            { exclude: ['password'] },
+        include: [
+            {
+                model: Category,
+                attributes: ['name'],
+                include: {
+                    model: Rating,
+                    attributes: ['rating', 'about_rating']
+                }
+                // include avatar here & possibly a user-bio
+            }
+        ]
 
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
     })
+        .then(async dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No users found' });
+                return;
+            }
+
+
+            for (var i = 0; i < dbUserData.length; i++) {
+                var total_rating = 0
+                for (var j = 0; j < dbUserData[i].categories.length; j++) {
+                    total_rating += dbUserData[i].categories[j].ratings[0].rating
+                }
+                var average_rating = total_rating / dbUserData[i].categories.length
+                console.log(average_rating)
+
+                // await User.update({overall: average_rating},{where: {
+                // id: dbUserData[i].id
+                //  }})
+                dbUserData[i].overall = average_rating
+                //dbUserData = await dbUserData[i].save()
+
+            }
+
+            const users = dbUserData.map(user => user.get({ plain: true }));
+
+            res.render('homepage', {
+                users,
+                loggedIn: req.session.loggedIn
+            });
+            console.log(users)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/user/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes:
+            { exclude: ['password'] },
+        include: [
+            {
+                model: Category,
+                attributes: ['name'],
+                include: {
+                    model: Rating,
+                    attributes: ['rating', 'about_rating']
+                }
+                // include avatar here & possibly a user-bio
+            }
+        ]
+
+    })
+        .then(async dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+
+            var total_rating = 0
+            if (!dbUserData.categories.length) {
+                var average_rating = 0
+            } else {
+                for (var i = 0; i < dbUserData.categories.length; i++) {
+                    total_rating += dbUserData.categories[i].ratings[0].rating
+                }
+                var average_rating = total_rating / dbUserData.categories.length
+                console.log(average_rating)
+                dbUserData.overall = average_rating
+                dbUserData = await dbUserData.save()
+            }
+
+            const user = dbUserData.get({ plain: true });
+
+            res.render('single-user', {
+                user,
+                loggedIn: req.session.loggedIn
+            });
+            console.log(user)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });;
 });
+
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
