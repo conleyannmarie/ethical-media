fc8e34bffb6ec1c4d3b0a89c5492c412d802c505
bhyland <blakehyland95@gmail.com>
2021-11-03 20:03:00 -0600

    added profile route

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const profileRoutes = require('./profile-routes.js');
+
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
---
+++ public/javascript/profile.js
@@ -0,0 +1,14 @@
+async function profile() {
+    const response = await fetch('/profile-routes', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#profile').addEventListener('click', profile);
