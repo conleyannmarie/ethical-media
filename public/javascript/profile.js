50cafae41d075f6976c523f1b3f88c8233f09aab
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-11-03 18:42:08 -0600

    merged

---
+++ public/javascript/profile.js
@@ -0,0 +1,14 @@
+// var imgEl = document.getElementById('profilePic')
+
+// function needs to go to user's profile by using the user's id that it clicked
+
+// async function ??
+
+// imgEl.addEventListener("click", function () {
+//     fetch('/api/users', {
+//         method: 'GET',
+
+//     })
+// })
+
+
