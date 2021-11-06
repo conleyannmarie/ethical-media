c44bdddf2758f815aec4639906fe8b14014d7027
Tom Hertig <joehertig94@gmail.com>
2021-11-05 21:58:26 -0600

    successfully returned proper amt after clicking star rating

--- public/javascript/stars.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".submit-rating");
-const thanksmsg = document.querySelector(".thanks-msg");
-const starRating = document.querySelector(".star-input");
-// Success msg show/hide
-btn.onclick = () => {
-  starRating.style.display = "none";
-  thanksmsg.style.display = "table";
-  return false;
-};
